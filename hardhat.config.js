e580065a727ad971a13ba466df0cc2c4c82e9da7
Kavindu Wickramasekara <kavi2020wick@gmail.com>
2024-08-13 12:57:02 +0530

    initial commit

---
+++ hardhat.config.js
@@ -0,0 +1,56 @@
+require("@nomicfoundation/hardhat-toolbox");
+require('dotenv').config()
+require("@nomiclabs/hardhat-etherscan");
+
+module.exports = {
+
+  solidity: "0.8.6",
+  // settings:{
+  //   optimizer:{
+  //     enabled:true,
+  //     runs:200
+  //   }
+  
+  networks:{
+    hardhat:{
+      chainId:1337
+    },
+    // polygonAmoy:{
+    //   url:`https://polygon-amoy.infura.io/v3/${process.env.INFURA_API}`,
+    //   accounts:[process.env.MAIN_ACCOUNT],
+    //   chainId:80002,
+    // },
+    sepolia:{
+      url:`https://sepolia.infura.io/v3/${process.env.INFURA_API}`,
+      accounts:[process.env.MAIN_ACCOUNT],
+      chainId:11155111,
+    },
+    //go to metamask and select mumbai testnet
+    //go to account details from the three dots option
+    //select export private key
+    //copy and paste pvt key .env Main_account 
+
+  }
+}
+    // goerli:{
+    //   url: `https://goerli.infura.io/v3/${process.env.INFURA_API}`,
+    //   accounts:[process.env.MAIN_ACCOUNT],
+    //   chainId: 5,
+    // },
+    // mumbai:{
+    //   url:`https://polygon-mumbai.infura.io/v3/${process.env.INFURA_API}`,
+    //   accounts:[process.env.MAIN_ACCOUNT],
+    //   chainId:80001,
+    // },
+
+
+  
+  // gasReporter:{
+  //   enabled:true,
+  //   currency:"USD",
+  //   coinmarketcap:process.env.COINMARKETCAP,
+  //   token:"matic",
+  //   outputFile:"gasReports.txt",
+  //   noColors:true
+  // }
+  
